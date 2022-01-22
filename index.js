fabb1494befe213bb1a79c14a3aa7c303a2f2a84
Andrea Mac <aleatrengrouse@gmail.com>
2022-01-22 21:00:42 +0000

    variable for planner and time format added

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ console.log(moment());
 $(function() {});
 
 
-// set variable for current day
+// current day
 var today = moment().format("dddd, MMMM Do");
 
-// set variable for current hour
+// current hour
 var now = moment().format("H A");
 
+// set variable for day planner
+// time must be in "H A" format
+var planDay = [
+	{ time: "9 AM", event: "" },
+	{ time: "10 AM", event: "" },
+	{ time: "11 AM", event: "" },
+	{ time: "12 PM", event: "" },
+	{ time: "1 PM", event: "" },
+	{ time: "2 PM", event: "" },
+	{ time: "3 PM", event: "" },
+	{ time: "4 PM", event: "" },
+	{ time: "5 PM", event: "" }
+];
